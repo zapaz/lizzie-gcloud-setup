b602b179adc805c83cd09b42a30ca430163bd35a
Alain Papazoglou <alain@eliga.fr>
2019-06-25 22:50:37 +0200

    config machine gcloud

--- config.sh
+++ config.sh
@@ -4,17 +4,17 @@
 # See https://cloud.google.com/compute/docs/gpus/add-gpus#create-new-gpu-instance for more details.
 
 # the name of the created instance
-INSTANCE_NAME=leelazero-v100
+INSTANCE_NAME=lila0
 
 # can be either "best" or "elf" for the facebook opengo converted weights (currently stronger, slower)
 NETWORK=best
 # NETWORK=elf
 
 # pick a nearby zone that has the gpu you want - see https://cloud.google.com/compute/docs/gpus/
-ZONE=europe-west4-a
+ZONE=europe-west1-b
 
 # can be k80, p100, v100 - see README.md
-GPU_TYPE=nvidia-tesla-v100
+GPU_TYPE=nvidia-tesla-k80
 
 # the number of gpus (one should usually be enough)
 GPU_COUNT=1
@@ -23,10 +23,10 @@ GPU_COUNT=1
 CPU_COUNT=6
 
 # 15GB should be plenty of space for just leela zero.. You might get away with less
-DISK_SIZE=15GB
+DISK_SIZE=10GB
 
 # Set the RAM here - not sure how much is needed for leela
-MEMORY=10GB
+MEMORY=16GB
 
 # Only touch these if you know what you're doing. You will need to adapt the setup script for other Linux distributions.
 IMAGE_FAMILY=ubuntu-1804-lts
